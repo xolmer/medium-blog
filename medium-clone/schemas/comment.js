456bbad37bc26d0fb42e629abc3360843e5fc3c1
Xolmer <xolmer@Xolmers-MacBook-Pro.local>
2022-07-21 21:37:17 +0200

    final

---
+++ medium-clone/schemas/comment.js
@@ -0,0 +1,30 @@
+export default {
+  name: "comment",
+  title: "Comment",
+  type: "document",
+  fields: [
+    {
+      name: "name",
+      type: "string",
+    },
+    {
+      title: "Approved",
+      name: "approved",
+      type: "boolean",
+      description: "Is this comment approved?",
+    },
+    {
+      name: "email",
+      type: "string",
+    },
+    {
+      name: "comment",
+      type: "text",
+    },
+    {
+      name: "post",
+      type: "reference",
+      to: [{ type: "post" }],
+    },
+  ],
+};
