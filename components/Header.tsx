541e712a96d12589fcfe046468e668fdcf1c8125
Xolmer <xolmer@Xolmers-MacBook-Pro.local>
2022-07-21 21:47:10 +0200

    final

--- components/Header.tsx
+++ components/Header.tsx
@@ -8,14 +8,23 @@ const Header = () => {
           <img src="/logo.svg" alt="logo" className="w-44 object-contain cursor-pointer" />
         </Link>
         <div className="hidden md:inline-flex items-center space-x-5">
-          <h3>About</h3>
-          <h3>Contact</h3>
-          <h3 className="text-white bg-purple-600 rounded-full px-4 py-1">Follow</h3>
+          <Link href="https://news-blog.sanity.studio/desk" className="cursor-pointer">
+            About
+          </Link>
+          <Link href="https://news-blog.sanity.studio/desk" className="cursor-pointer">
+            Contact
+          </Link>
+          <h3 className="text-white bg-purple-600 rounded-full px-4 py-1 cursor-pointer">Follow</h3>
         </div>
       </div>
       <div className="flex items-center space-x-5 text-purple-900">
-        <h3>Sign In</h3>
-        <h3 className="border px-4 py-1 rounded-full border-purple-600">Get Started</h3>
+        <Link href="https://news-blog.sanity.studio/desk">
+          <h3 className="cursor-pointer">Sign In</h3>
+        </Link>
+
+        <Link href="https://news-blog.sanity.studio/desk">
+          <h3 className="cursor-pointer border px-4 py-1 rounded-full border-purple-600">Get Started</h3>
+        </Link>
       </div>
     </header>
   );
